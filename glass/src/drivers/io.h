43ff21a21f8b0353b2b4f4c9f114378f1351da91
rizet <ryzenupp@hotmail.com>
2021-05-18 09:11:51 -0400

    perf: set always_inline attribute on in*/out* functions

--- glass/src/drivers/io.h
+++ glass/src/drivers/io.h
@@ -1,14 +1,16 @@
 #pragma once
 #include <stdint.h>
 
-static inline void outb(uint16_t port, uint8_t val)
+__attribute__((always_inline)) 
+void outb(uint16_t port, uint8_t val)
 {
     asm volatile("outb %0, %1"
                 :
                 : "a"(val), "Nd"(port));
 }
 
-static inline uint8_t inb(uint16_t port)
+__attribute__((always_inline)) 
+uint8_t inb(uint16_t port)
 {
     uint8_t ret;
     asm volatile("inb %1, %0"
@@ -16,15 +18,16 @@ static inline uint8_t inb(uint16_t port)
                 : "Nd"(port));
     return ret;
 }
-
-static inline void outw(uint16_t port, uint16_t val)
+__attribute__((always_inline)) 
+void outw(uint16_t port, uint16_t val)
 {
     asm volatile("outw %0, %1"
                 :
                 : "a"(val), "Nd"(port));
 }
 
-static inline uint16_t inw(uint16_t port)
+__attribute__((always_inline)) 
+uint16_t inw(uint16_t port)
 {
     uint16_t ret;
     asm volatile("inw %1, %0"
@@ -33,14 +36,16 @@ static inline uint16_t inw(uint16_t port)
     return ret;
 }
 
-static inline void outl(uint16_t port, uint32_t val)
+__attribute__((always_inline)) 
+void outl(uint16_t port, uint32_t val)
 {
     asm volatile("outl %0, %1"
                 :
                 : "a"(val), "Nd"(port));
 }
 
-static inline uint32_t inl(uint16_t port)
+__attribute__((always_inline)) 
+uint32_t inl(uint16_t port)
 {
     uint32_t ret;
     asm volatile("inl %1, %0"
