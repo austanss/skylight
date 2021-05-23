e0ff30b3d7020724669cb6bd7781833ab07471b2
rizet <ryzenupp@hotmail.com>
2021-05-23 16:31:29 -0400

    fix: map kernel pages

--- glass/src/mm/paging/reload.c
+++ glass/src/mm/paging/reload.c
@@ -21,5 +21,8 @@ void paging_reload(struct stivale2_struct_tag_memmap* map) {
                 for (uint64_t t = map->memmap[i].base; t < map->memmap[i].base + map->memmap[i].length; t+=PAGING_PAGE_SIZE)
                     paging_map_page((void *)(t + PAGING_VIRTUAL_OFFSET), (void *)t, PAGING_FLAGS_KERNEL_PAGE);
 
+    for (uint64_t t = (uint64_t)&__load_base; t < (uint64_t)&__load_max; t+=PAGING_PAGE_SIZE)
+        paging_map_page((void *)t, (void *)(t - PAGING_KERNEL_OFFSET), PAGING_FLAGS_KERNEL_PAGE);
+
     asm volatile ("mov %0, %%cr3" : : "a"(pml4));
 }
