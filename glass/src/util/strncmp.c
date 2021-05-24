928c2626f184d13eb3c85a794bcdb11bd1f5ae90
rizet <ryzenupp@hotmail.com>
2021-05-23 23:13:15 -0400

    fix: non-standard strncmp behavior

--- glass/src/util/strncmp.c
+++ glass/src/util/strncmp.c
@@ -2,7 +2,7 @@
 #include <stddef.h>
 
 int strncmp(const char* __s1, const char* __s2, size_t __n) {
-    for (size_t i = 0; *__s1 && i < __n; __s1++) {
+    for (size_t i = 0; *__s1 && i < __n; __s1++, i++) {
 		if (*__s1 != *__s2)
 			return *__s1 - *__s2;
 		__s2++;
