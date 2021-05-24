3b3ccddaaebabef9294842784afffd21a746afb3
rizet <ryzenupp@hotmail.com>
2021-05-24 10:39:36 -0400

    partial: disable the 8259

--- glass/src/drivers/apic/initialize.c
+++ glass/src/drivers/apic/initialize.c
@@ -1,6 +1,8 @@
 #include "../acpi/tables/madt.h"
-#include "../uart/serial.h"
+#include "../8259/pic.h"
 
 void apic_initialize() {
     acpi_madt_header_t* madt = ACPI_MADT_GET();
+
+    pic_disable();
 }
