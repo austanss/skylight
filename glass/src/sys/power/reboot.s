81d63f011ebcd6013053771611265885b0a89d6b
rizet <ryzenupp@hotmail.com>
2021-05-29 17:18:04 -0400

    feat: 8042 cpu reset

---
+++ glass/src/sys/power/reboot.s
@@ -0,0 +1,14 @@
+global reboot
+reboot:
+    xor eax, eax
+    mov ah, 0x02
+    mov dx, 0x0064
+    .waiting:
+    in al, dx
+    and al, ah
+    test al, al
+    jnz .waiting
+    .reset:
+    mov ah, 0xFE
+    out dx, al
+    hlt
