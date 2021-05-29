92a00ce36bedc6a3e1cbd1be1df6ae7343e6cb15
DMHSW <6974902+gek169@users.noreply.github.com>
2021-05-28 20:26:15 -0400

    Update debug.h

--- glass/src/debug.h
+++ glass/src/debug.h
@@ -4,3 +4,17 @@
 #ifdef __DEBUG__
 #define DEBUG_PRINT(x, y) serial_terminal()->put##y(x);
 #endif
+
+#ifndef NDEBUG
+/*dont let the compiler optimize out my variable, please!*/
+#define DBG_VAR volatile
+/*static_assert is valid in C11 through gnu C extensions which are used by default in clang.*/
+#define GLASS_ASSERT(cond) static_assert(cond, "Compiletime test failed");
+#warning "file" __FILE__ "is compiling with compiletime and runtime debugging features enabled! Expect lower performance".
+
+#else
+
+#define DBG_VAR /*a comment.*/
+#define GLASS_ASSERT(cond) /*another comment*/
+
+#endif
