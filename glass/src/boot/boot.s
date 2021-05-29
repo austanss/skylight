f249ccf9175ea6082e2224bb97b68ec425d0c76f
rizet <ryzenupp@hotmail.com>
2021-05-29 13:10:21 -0400

    perf: zero registers upon cpl3 entry

--- glass/src/boot/boot.s
+++ glass/src/boot/boot.s
@@ -104,4 +104,21 @@ userspace:
     push rbp
     mov rbp, rsp
 
+    xor rax, rax
+    xor rbx, rbx
+    xor rcx, rcx
+    xor rdx, rdx
+
+    xor rdi, rdi
+    xor rsi, rsi
+
+    xor r8, r8
+    xor r9, r9
+    xor r10, r10
+    xor r11, r11
+    xor r12, r12
+    xor r13, r13
+    xor r14, r14
+    xor r15, r15
+
     jmp $
