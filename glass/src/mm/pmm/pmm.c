3c79e2538ad622f85aac9748d91b35ede1b190b0
rizet <ryzenupp@hotmail.com>
2021-05-30 12:21:16 -0400

    looks: more verbose memory map value output

--- glass/src/mm/pmm/pmm.c
+++ glass/src/mm/pmm/pmm.c
@@ -5,6 +5,20 @@
 #include <string.h>
 #include <stdlib.h>
 
+static char* memtype_string(uint32_t type) {
+    switch (type) {
+        case STIVALE2_MMAP_USABLE: return "free";
+        case STIVALE2_MMAP_BAD_MEMORY: return "bad";
+        case STIVALE2_MMAP_ACPI_RECLAIMABLE: return "used";
+        case STIVALE2_MMAP_RESERVED: return "used";
+        case STIVALE2_MMAP_BOOTLOADER_RECLAIMABLE: return "used";
+        case STIVALE2_MMAP_ACPI_NVS: return "dangerous";
+        case STIVALE2_MMAP_FRAMEBUFFER: return "framebuffer";
+        case STIVALE2_MMAP_KERNEL_AND_MODULES: return "kernel";
+    }
+    return "unidentifiable";
+}
+
 extern bool pfa_allowing_allocations;
 bool pfa_allowing_allocations = false;
 extern uint8_t* allocation_map;
@@ -79,11 +93,14 @@ void pmm_start(struct stivale2_struct_tag_memmap* memory_map_info) {
 
     free_memory = 0;
 
+    serial_terminal()->puts("\n\nmemory map:\n\n");
     for (uint64_t i = 0; i < memory_map_info->entries; i++) {
         if (memory_map_info->memmap[i].type == 1)
             pmm_unlock_pages((void *)(memory_map_info->memmap[i].base + PAGING_VIRTUAL_OFFSET), memory_map_info->memmap[i].length / PAGING_PAGE_SIZE);
 
-        serial_terminal()->putul(memory_map_info->memmap[i].type)->putc(' ')->putul(memory_map_info->memmap[i].length / PAGING_PAGE_SIZE)->putc('\n');
+        serial_terminal()->puts("base: ")->putul(memory_map_info->memmap[i].base)->putc(',');
+        serial_terminal()->puts(" type: ")->puts(memtype_string(memory_map_info->memmap[i].type))->putc(',');
+        serial_terminal()->puts(" pages: ")->putd(memory_map_info->memmap[i].length / PAGING_PAGE_SIZE)->putc('\n');
     }
 
     pmm_lock_pages((void *)(0x0 + PAGING_VIRTUAL_OFFSET), 0x100000 / PAGING_PAGE_SIZE);
