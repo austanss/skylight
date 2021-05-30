35e628113ddb1a7945c0aa2734c22f1d6cb3f01c
rizet <ryzenupp@hotmail.com>
2021-05-30 12:40:39 -0400

    looks: kapic configuration loggging

--- glass/src/drivers/apic/initialize.c
+++ glass/src/drivers/apic/initialize.c
@@ -22,11 +22,15 @@ static void detect_apic() {
 }
 
 static void configure_local_apic(acpi_madt_header_t* madt) {
+    serial_terminal()->puts("\n\nlocal apic configuration:\n\n");
     apic_local_set_base((void *)(uintptr_t)madt->lapic_address);
+    serial_terminal()->puts("base: ")->putul(madt->lapic_address)->putc('\n');
 
     apic_local_write(APIC_LOCAL_REGISTER_SPURIOUS_INT_VECTOR, 0x100 | apic_local_read(APIC_LOCAL_REGISTER_SPURIOUS_INT_VECTOR));
+    serial_terminal()->puts("spiv: ")->putul(apic_local_read(APIC_LOCAL_REGISTER_SPURIOUS_INT_VECTOR))->putc('\n');
 
     uint32_t lapic_id = apic_local_read(APIC_LOCAL_REGISTER_ID);
+    serial_terminal()->puts("id: ")->putul(lapic_id)->putc('\n');
 
     const uint8_t nmi = 0x04 & 0x07;
 
@@ -55,6 +59,8 @@ static void configure_local_apic(acpi_madt_header_t* madt) {
             apic_local_write(lvt, entry);
         }
     }
+    serial_terminal()->puts("lint0: ")->putul(apic_local_read(APIC_LOCAL_REGISTER_LVT_LINT0))->putc('\n');
+    serial_terminal()->puts("lint1: ")->putul(apic_local_read(APIC_LOCAL_REGISTER_LVT_LINT1))->putc('\n');
 }
 
 static void configure_io_apic(acpi_madt_header_t* madt) {
@@ -70,6 +76,8 @@ static void configure_io_apic(acpi_madt_header_t* madt) {
         }
     }
 
+    serial_terminal()->puts("\n\nioapic overrides:\n\n");
+
     for (acpi_madt_record_t* madt_record = (acpi_madt_record_t *)((uintptr_t)madt + sizeof(acpi_madt_header_t));;) {
         if (((uintptr_t)madt_record - (uintptr_t)madt) >= madt->common.length)
             break;
@@ -82,6 +90,8 @@ static void configure_io_apic(acpi_madt_header_t* madt) {
                                 (uint8_t)override->gsi, 
                                 !!(override->flags & ACPI_MADT_RECORD_ISO_NMI_FLAG_ACTIVE_LOW), 
                                 !!(override->flags & ACPI_MADT_RECORD_ISO_NMI_FLAG_LEVEL_TRIGGERED));
+
+            serial_terminal()->puts("irq: ")->putul(override->irq_source)->puts(", vec: ")->putul(override->gsi)->putc('\n');
         }
     }
 }
