019ea46d769a9e3b098bc542f0a3732992a03209
austanss <ryzenupp@hotmail.com>
2021-11-28 12:04:27 -0500

    fix: nasty strcmp bug

--- glass/src/misc/strcmp.c
+++ glass/src/misc/strcmp.c
@@ -5,7 +5,7 @@ int strcmp(const char* __s1, const char* __s2) {
 	const signed char* __ss2 = (const signed char *)__s2;
     for (; *__ss1; __ss1++) {
 		if (*__ss1 != *__ss2)
-			return (signed int)(*__s1 - *__s2);
+			return (signed int)(*__ss1 - *__ss2);
 		__ss2++;
 	}
 	return 0;
