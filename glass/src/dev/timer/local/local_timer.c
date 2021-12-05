48ce9dfe074435e0b6c35092a313bf2c224e2219
austanss <ryzenupp@hotmail.com>
2021-12-05 17:08:05 -0500

    feat: complete local timer functions

--- glass/src/dev/timer/local/local_timer.c
+++ glass/src/dev/timer/local/local_timer.c
@@ -4,9 +4,11 @@
 #include "dev/timer/local/local_timer.h"
 #include "dev/apic/lapic.h"
 #include "cpu/interrupts/idt.h"
+#include "cpu/tss/tss.h"
 
 static uint64_t tpms;
 static bool calibrated = false;
+static uint8_t vector;
 
 __attribute__ ((interrupt))
 void __local_timer_builtin_handler(void* frame) {
@@ -15,16 +17,18 @@ void __local_timer_builtin_handler(void* frame) {
 }
 
 void local_timer_calibrate() {
-    apic_local_write(APIC_LOCAL_REGISTER_DIVIDE_CONFIG, 1);
+    apic_local_write(APIC_LOCAL_REGISTER_DIVIDE_CONFIG, LOCAL_TIMER_DIVISOR_001);
 
-    uint32_t lvt_descriptor = 0x00000000;
-    uint8_t vector = idt_allocate_vector();
+    uint32_t lvt_descriptor = 0x00000000 | (0x01 << 16);
+    vector = idt_allocate_vector();
 
     lvt_descriptor |= vector;
 
     apic_local_write(APIC_LOCAL_REGISTER_LVT_TIMER, lvt_descriptor);
 
     apic_local_write(APIC_LOCAL_REGISTER_INITIAL_COUNT, 0xFFFFFFFF);
+
+    local_timer_set_handler(__local_timer_builtin_handler);
 
     pit_enable();
     pit_deadline_wait(PIT_FREQUENCY / 1000);
@@ -34,10 +38,19 @@ void local_timer_calibrate() {
     pit_disable();
 
     asm ("hlt");
+
+    calibrated = true;
 }
 
 uint64_t local_timer_get_tpms() {
     return tpms;
 }
 
+void local_timer_set_handler(void (*handler)) {
+    idt_set_descriptor(vector, (uintptr_t)handler, IDT_DESCRIPTOR_X32_INTERRUPT, TSS_IST_ROUTINE);
+}
 
+void local_timer_set_frequency(uint64_t hz) {
+    uint64_t counts_per_tick = (tpms * 1000) / hz;
+    apic_local_write(APIC_LOCAL_REGISTER_INITIAL_COUNT, counts_per_tick);
+}
--- glass/src/dev/timer/local/local_timer.h
+++ glass/src/dev/timer/local/local_timer.h
@@ -1,6 +1,15 @@
 #pragma once
 #include <stdint.h>
 
+#define LOCAL_TIMER_DIVISOR_002 0x00
+#define LOCAL_TIMER_DIVISOR_004 0x01
+#define LOCAL_TIMER_DIVISOR_008 0x02
+#define LOCAL_TIMER_DIVISOR_016 0x03
+#define LOCAL_TIMER_DIVISOR_032 0x04
+#define LOCAL_TIMER_DIVISOR_064 0x05
+#define LOCAL_TIMER_DIVISOR_128 0x06
+#define LOCAL_TIMER_DIVISOR_001 0x07
+
 void local_timer_calibrate();
 void local_timer_set_frequency(uint64_t hz);
 
